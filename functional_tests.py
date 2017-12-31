757dbd1a30674ac960f9d5e6a95ffa816cd3c43c
Paweł Merdała <p.merdala@mgwccg.pl>
2017-12-31 19:07:59 +0100

    pierwszy test

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,11 @@
+#-*- coding: utf8 -*-
 from selenium import webdriver
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
 		self.browser.quit()
