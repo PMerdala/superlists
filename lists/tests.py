16a23730b133e4c9c96490a9f5f7ced8e8d6aef2
Paweł Merdała <p.merdala@mgwccg.pl>
2017-12-31 21:38:34 +0100

    Refaktoryzacja widoku strony głównej w celu użycia szablonu

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
-
+from django.template.loader import render_to_string
 from lists.views import home_page
 
 # Create your tests here.
@@ -14,6 +14,11 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
+		#sprawdzenie czy szablon jest choć trochę zgodny z założeniem
+		#gdybym zrezygnował z tego sprawdzenia mógłbym w szablon wpisać cokolwiek
 		self.assertTrue(response.content.startswith(b'<html>'))
 		self.assertIn(b'<title>Listy rzeczy do zrobienia</title>',response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))
+		self.assertTrue(response.content.strip().endswith(b'</html>'))
+		#sprawdzenie czy wynik pobrania strony jest zgodny z szablonem
+		expected_html = render_to_string('home.html')
+		self.assertEqual(response.content.decode(),expected_html)
